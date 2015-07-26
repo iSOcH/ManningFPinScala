3a3afada3f0e9c7364896660cb59ead1a8312851
iSOcH <iso@thinktux.net>
2015-07-26 22:07:34 +0200

    exercises in Chap05 (next: ex05_13, zipWith and zipAll)

--- src/main/scala/ch/pascalschwarz/manningfpinscala/Chap05Streams/Stream.scala
+++ src/main/scala/ch/pascalschwarz/manningfpinscala/Chap05Streams/Stream.scala
@@ -9,6 +9,15 @@ sealed trait Stream[+A] {
     case _ => None
   }
 
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case Cons(h,t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
+
+  def existsFold(p: A => Boolean): Boolean = {
+    foldRight(false)(p(_) || _)
+  }
+
   // ex05_01
   def toListRec: List[A] = this match {
     case Cons(h, t) => h() :: t().toListRec
@@ -50,6 +59,58 @@ sealed trait Stream[+A] {
     case Empty => false
     case Cons(h, t) => if (p(h())) true else t() exists p
   }
+
+  // ex05_04
+  def forAll(p: A => Boolean): Boolean = {
+    foldRight(true)(p(_) && _)
+  }
+
+  // ex05_05
+  def takeWhileFold(p: A => Boolean): Stream[A] = {
+    foldRight(Stream.empty[A]){ (h, t) =>
+      if (p(h)) Stream.cons(h, t)
+      else Stream.empty
+    }
+  }
+
+  // ex05_06
+  def headOptionFold: Option[A] = {
+    foldRight(None: Option[A])((h, _) => Some(h))
+  }
+
+  // ex05_07
+  def map[B](f: A => B): Stream[B] = {
+    foldRight(Stream.empty[B]){(h,t) => Stream.cons(f(h), t)} // t is already mapped over
+  }
+  def filter(p: A => Boolean): Stream[A] = {
+    foldRight(Stream.empty[A]){(h,t) =>
+      if (p(h)) Stream.cons(h, t)
+      else t // t is already filtered!
+    }
+  }
+  def append[B >: A](other: => Stream[B]): Stream[B] = {
+    foldRight(other){(h,t) => Stream.cons(h, t)}
+  }
+  def flatMap[B](f: A => Stream[B]): Stream[B] = {
+    foldRight(Stream.empty[B]){ (h,t) => f(h).append(t) }
+  }
+
+  // ex05_13
+  def mapUF[B](f: A => B): Stream[B] = {
+    Stream.unfold(this){
+      case Cons(h,t) => Some{ (f(h()), t()) }
+      case Empty => None
+    }
+  }
+  def takeUF(n: Int): Stream[A] = Stream.unfold((this,n)) {
+    case (Cons(h, _), 1) => Some{ (h(), (Stream.empty, 0)) }
+    case (Cons(h, t), n) if n > 1 => Some{ (h(), (t(), (n-1))) }
+    case _ => None
+  }
+  def takeWhileUF(p: A => Boolean) = Stream.unfold(this) {
+    case Cons(h, t) if p(h()) => Some{ (h(), t()) }
+    case _ => None
+  }
 }
 case class Cons[+A](head: () => A, tail: () => Stream[A]) extends Stream[A]
 case object Empty extends Stream[Nothing]
@@ -65,5 +126,36 @@ object Stream {
   def apply[A](as: A*): Stream[A] = {
     if (as.isEmpty) Empty
     else cons(as.head, apply(as.tail: _*))
+  }
+
+  // ex05_08
+  def constant[A](a: A): Stream[A] = cons(a, constant(a))
+
+  // ex05_09
+  def from(n: Int): Stream[Int] = cons(n, from(n+1))
+
+  // ex05_10
+  def fibs: Stream[Int] = {
+    def make(n1: Int, n2: Int): Stream[Int] = cons(n1+n2, make(n2, n1+n2))
+    make(0,1)
+  }
+
+  // ex05_11
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = {
+    f(z).map{
+      case (a,s) => cons(a, unfold(s)(f))
+    }.getOrElse(empty)
+  }
+
+  // ex05_12
+  def constantUnfold[A](a: A): Stream[A] = {
+    unfold(())(_ => Some(a, ())) // State is Unit - there is no state
+  }
+  def onesUnfold: Stream[Int] = constantUnfold(1)
+  def fromUnfold(n: Int): Stream[Int] = unfold(n-1){
+    i => Some(i+1,i+1)
+  }
+  def fibsUnfold: Stream[Int] = unfold((0,1)){
+    case (n1,n2) => Some{ (n1+n2, (n2, n1+n2)) }
   }
 }
