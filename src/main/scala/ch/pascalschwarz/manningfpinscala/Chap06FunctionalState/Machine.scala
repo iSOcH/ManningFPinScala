04cf30bce194f1e935714017f625329941bfd9bc
iSO <iso@thinktux.net>
2015-08-09 14:04:59 +0200

    Chap06: ex11 alternative

--- src/main/scala/ch/pascalschwarz/manningfpinscala/Chap06FunctionalState/Machine.scala
+++ src/main/scala/ch/pascalschwarz/manningfpinscala/Chap06FunctionalState/Machine.scala
@@ -14,16 +14,25 @@ object Candy {
   def update = (i: Input) => (s: Machine) => (i, s) match {
     case (_, Machine(_, 0, _)) => s // "a machine thats out of candy ignores all inputs"
     case (Turn, Machine(true, _, _)) => s // "turning the knob on a locked machine...
-    case (Coin, Machine(false, _, _)) => s // ...or inserting a coin in an unlocked machine does nothing"
-    case (Coin, Machine(_, candy, coins)) => Machine(false, candy, coins + 1) // false: if candy = 0, first case matches
-    case (Turn, Machine(false, candy, coins)) => Machine(true, candy - 1, coins)
+    case (Coin, Machine(false, _, coins)) => s // ...or inserting a coin in an unlocked machine does nothing"
+    case (Coin, Machine(_, candy, coins)) => Machine(locked = false, candy, coins + 1) // false: if candy = 0, first case matches
+    case (Turn, Machine(false, candy, coins)) => Machine(locked = true, candy - 1, coins)
   }
 
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int,Int)] = State{(machine: Machine) =>
-    val actions = State.sequence{
-      inputs.map(update andThen State.modify[Machine])
-    }
-    val (_, finalMachine) = actions.run(machine)
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int,Int)] = State{ machine =>
+    val actions = inputs.map(update andThen State.modify)
+    val (_, finalMachine) = State.sequence(actions).run(machine)
     ((finalMachine.coins, finalMachine.candies), finalMachine)
   }
+
+  def simulateMachine2(inputs: List[Input]): State[Machine, (Int,Int)] = State{ machine =>
+    val actions = inputs.map(update)
+    val finalMachine = actions.foldLeft(machine)((m, f) => f(m))
+    ((finalMachine.coins, finalMachine.candies), finalMachine)
+  }
+
+  def simulateMachineSol(inputs: List[Input]): State[Machine, (Int, Int)] = for {
+    _ <- State.sequence(inputs map (State.modify[Machine] _ compose update))
+    s <- State.get
+  } yield (s.coins, s.candies)
 }
