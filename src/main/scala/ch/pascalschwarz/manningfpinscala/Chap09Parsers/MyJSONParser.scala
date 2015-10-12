7a2d1c83043bfd7da3b961eb016c6da7001aa69d
iSOcH <iso@thinktux.net>
2015-10-12 11:59:05 +0200

    chap09 started ex 9.9

---
+++ src/main/scala/ch/pascalschwarz/manningfpinscala/Chap09Parsers/MyJSONParser.scala
@@ -0,0 +1,32 @@
+package ch.pascalschwarz.manningfpinscala.Chap09Parsers
+
+import ch.pascalschwarz.manningfpinscala.Chap09Parsers.JSON.{JString, JNumber, JBool, JNull}
+
+// ex 09_09
+object MyJSONParser {
+  def jsonParser[Err, Parser[+_]](P: Parsers[Err, Parser]): Parser[JSON] = {
+    import P._
+    val spaces = char(' ').many.slice
+
+    val jsonNull: Parser[JNull.type] = string("null").slice.map(_ => JNull)
+
+    // TODO: NaN, Infinity
+    val jsonNumber: Parser[JNumber] = (regex("-?[0-9]+\\.?[0-9]*".r) or regex("-?\\.[0-9]+".r)) map (d => JNumber(d.toDouble))
+
+    // we might want to handle toBoolean error and result in parse-fail... but it should not fail in these cases
+    val jsonBool: Parser[JBool] = (string("true") or string("false")) map (b => JBool(b.toBoolean))
+
+
+    object stringParsing {
+      val unicode:Parser[String] =  (string("\\u").slice ** regex("[0-9a-fA-F]{4}".r)) map (u => Character.toChars(Integer.parseInt(u._2, 16)).toString)
+      val escapedChar:Parser[String] = (char('\\').slice ** regex("."r)) map (_._2.charAt(1).toString)
+      val regularString:Parser[String] = regex("[^\"\\]*"r)
+    }
+    val jsonString: Parser[JString] =
+      (char('"') **
+        (stringParsing.unicode | stringParsing.escapedChar | stringParsing.regularString).many **
+      char('"')).map(s => JString(s._1._2.mkString))
+
+    ???
+  }
+}
